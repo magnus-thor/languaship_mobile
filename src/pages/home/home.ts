a0646a00ede7f1b686d2cd36807085ca4b6b2652
Antonella Morittu <morittuantonella@gmail.com>
2018-04-05 14:24:35 +0200

    add card to homepage view add dummy profile to home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  users: any
+  user: any
   constructor(public navCtrl: NavController) {
-
+    this.user =
+    {
+      id: 43,
+      type: "users",
+      attributes: {
+        name: "Homer Simpson",
+        city: "Springfield",
+        age: 40,
+        profileImage: "https://url-to-som-stock-photo",
+        languages: [
+          {
+            name: "English",
+            isoCode: "ENG",
+            proficiency: "native"
+          }
+        ]
+      }
+    }
+    this.users =[
+      { id: 2, type: "users", attributes: [] },
+      { id: 3, type: "users", attributes: [] },
+      { id: 4, type: "users", attributes: [] }
+    ];
   }
-
 }
