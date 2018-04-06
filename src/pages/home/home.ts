a67014d261944c8c02e03db76a3c28651179448d
Isabelle <isabellelidberg@live.se>
2018-04-06 16:12:14 +0200

    adds user provider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UsersProvider } from '../../providers/users/users';
 
 @Component({
   selector: 'page-home',
@@ -8,52 +9,18 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   users: any
-  
-  constructor(public navCtrl: NavController) {
 
-    this.users =[
-      { id: 1, type: "users", attributes: {
-        name: "Homer Simpson",
-        city: "Springfield",
-        age: 40,
-        profileImage: "http://cdn2.ubergizmo.com/wp-content/uploads/2016/02/homer-simpson.jpeg",
-          languages: [
-            {
-              name: "English",
-              isoCode: "ENG",
-              proficiency: "native"
-            }
-          ]
-        }
-      },
-      { id: 2, type: "users", attributes: {
-        name: "Super Mario",
-        city: "Rome",
-        age: 30,
-        profileImage: "https://www.techprincess.it/wp-content/uploads/2018/03/TechPrincess_MAR10Day_festeggiamenti-382x420.jpg",
-          languages: [
-            {
-              name: "Italian",
-              isoCode: "ITA",
-              proficiency: "native"
-            }
-          ]
-        }
-      },
-      { id: 3, type: "users", attributes: {
-        name: "Obelix",
-        city: "Paris",
-        age: 25,
-        profileImage: "http://www.asterix.com/imgs/perso3-3.png",
-          languages: [
-            {
-              name: "French",
-              isoCode: "FR",
-              proficiency: "native"
-            }
-          ]
-        }
-      },
-    ];
-  }
+  constructor(public navCtrl: NavController, private usersProvider: UsersProvider) {
+
+    this.users = usersProvider.getAll();
+//     this.users = this.getUsers();
+//
 }
+//
+//   getUsers(): void{
+//     this.usersProvider.getAll()
+//     .subscribe(users =>{
+//     this.users = users
+//     })
+  }
+// }
---
+++ src/providers/users/users.ts
@@ -0,0 +1,61 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class UsersProvider {
+
+  constructor() {
+    // console.log('Hello UsersProvider Provider');
+  }
+
+  getAll() {
+    return [
+      {
+        id: 1, type: "users", attributes: {
+          name: "Homer Simpson",
+          city: "Springfield",
+          age: 40,
+          profileImage: "http://cdn2.ubergizmo.com/wp-content/uploads/2016/02/homer-simpson.jpeg",
+          languages: [
+            {
+              name: "English",
+              isoCode: "ENG",
+              proficiency: "native"
+            }
+          ]
+        }
+      },
+      {
+        id: 2, type: "users", attributes: {
+          name: "Super Mario",
+          city: "Rome",
+          age: 30,
+          profileImage: "https://www.techprincess.it/wp-content/uploads/2018/03/TechPrincess_MAR10Day_festeggiamenti-382x420.jpg",
+          languages: [
+            {
+              name: "Italian",
+              isoCode: "ITA",
+              proficiency: "native"
+            }
+          ]
+        }
+      },
+      {
+        id: 3, type: "users", attributes: {
+          name: "Obelix",
+          city: "Paris",
+          age: 25,
+          profileImage: "http://www.asterix.com/imgs/perso3-3.png",
+          languages: [
+            {
+              name: "French",
+              isoCode: "FR",
+              proficiency: "native"
+            }
+          ]
+        }
+      },
+    ];
+  }
+}
