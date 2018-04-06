506f101914f68b8cd641d2b5d6cd4717e26e3965
Antonella Morittu <morittuantonella@gmail.com>
2018-04-06 10:08:07 +0200

    add drop-down menu with languages

--- src/pages/languages/languages.ts
+++ src/pages/languages/languages.ts
@@ -1,25 +1,28 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the LanguagesPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-languages',
   templateUrl: 'languages.html',
 })
+
 export class LanguagesPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  languages: any
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.languages =[
+          'ENGLISH',
+          'SPANISH',
+          'ITALIAN',
+          'DUTCH',
+          'SWEDISH',
+          'FRENCH'
+    ]
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad LanguagesPage');
-  }
-
+    }
 }
