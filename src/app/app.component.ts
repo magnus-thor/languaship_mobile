e0aade2ef8d11a82f973eee855b5576791319a78
Isabelle <isabellelidberg@live.se>
2018-04-11 11:28:51 +0200

    makes new sign up user to stay loged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, Events } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
@@ -27,12 +27,17 @@ export class MyApp {
     StatusBar, public splashScreen:
     SplashScreen,
     private _tokenService: Angular2TokenService,
-    private alertCtrl: AlertController
+    private alertCtrl: AlertController,
+    private event: Events
   ) {
 
     this._tokenService.init({
       apiBase: 'https://languaship.herokuapp.com/api/v1'
   });
+
+    this.event.subscribe('userSignedUp', (user) => {
+      this.currentUser = user;
+    })
 
     this.initializeApp();
 
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild  } from '@angular/core';
-import { IonicPage, NavController, NavParams, ViewController, Nav } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController, Nav, Events } from 'ionic-angular';
 import { AlertController } from 'ionic-angular';
 import { Angular2TokenService } from 'angular2-token';
 
@@ -24,7 +24,8 @@ export class SignupPage {
     public navCtrl: NavController,
     public navParams: NavParams,
     private _tokenService: Angular2TokenService,
-    private alertCtrl: AlertController
+    private alertCtrl: AlertController,
+    private event: Events
   ) {
 
       this.locations = [
@@ -103,7 +104,7 @@ export class SignupPage {
           text: 'Signup',
           handler: data => {
             this.register(data);
-            this.login(data);
+            // this.login(data);
           }
         }
       ]
@@ -111,62 +112,71 @@ export class SignupPage {
     confirm.present();
   }
 
-  loginPopUp() {
-      console.log('popup');
-      let confirm = this.alertCtrl.create({
-
-        title: 'Login',
-        inputs: [
-          {
-            name: 'email',
-            placeholder: 'email'
-          },
-          {
-            name: 'password',
-            placeholder: 'password',
-            type: 'password'
-          }
-        ],
-        buttons: [
-          {
-            text: 'Cancel',
-            handler: data => {
-              console.log('Cancel clicked');
-            }
-          },
-          {
-            text: 'Login',
-            handler: data => {
-              this.login(data);
-            }
-          }
-        ]
-      });
-      confirm.present();
-    }
+  // loginPopUp() {
+  //     console.log('popup');
+  //     let confirm = this.alertCtrl.create({
+  //
+  //       title: 'Login',
+  //       inputs: [
+  //         {
+  //           name: 'email',
+  //           placeholder: 'email'
+  //         },
+  //         {
+  //           name: 'password',
+  //           placeholder: 'password',
+  //           type: 'password'
+  //         }
+  //       ],
+  //       buttons: [
+  //         {
+  //           text: 'Cancel',
+  //           handler: data => {
+  //             console.log('Cancel clicked');
+  //           }
+  //         },
+  //         {
+  //           text: 'Login',
+  //           handler: data => {
+  //             this.login(data);
+  //           }
+  //         }
+  //       ]
+  //     });
+  //     confirm.present();
+  //   }
 
   register(credentials) {
     this._tokenService
       .registerAccount(credentials)
       .subscribe(
-        res => (this.currentUser = res.json().data),
+        res => {
+          (this.currentUser = res.json().data)
+          this.showAlert();
+          this.event.publish( 'userSignedUp', this.currentUser);
+          // this.NavController.push()
+        },
         err => console.error('error')
       );
   }
 
-  login(credentials) {
-    this._tokenService
-      .signIn(credentials)
-      .subscribe(
-        res => (this.currentUser = res.json().data),
-        err => console.error('error')
-      );
-  }
-
-  logout() {
-    this._tokenService
-      .signOut()
-      .subscribe(res => console.log(res), err => console.error('error'));
-    this.currentUser = undefined;
-  }
+  // login(credentials) {
+  //   this._tokenService
+  //     .signIn(credentials)
+  //     .subscribe(
+  //       res => {
+  //         (this.currentUser = res.json().data)
+  //         this.showAlert();
+  //         this.event.publish( 'userSignedUp', this.currentUser);
+  //       },
+  //       err => console.error('error')
+  //     );
+  // }
+  //
+  // logout() {
+  //   this._tokenService
+  //     .signOut()
+  //     .subscribe(res => console.log(res), err => console.error('error'));
+  //   this.currentUser = undefined;
+  // }
 }
